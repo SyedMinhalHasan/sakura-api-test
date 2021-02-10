5909be5bc800ca68aceabefe8872fc287ad13fb1
Syed Minhal Hasan <minhalhasan9@gmail.com>
2021-02-10 12:59:53 +0500

    change in typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,7 +9,4 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   database: 'd6aql6t7nf8sf0',
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   synchronize: true,
-  extra: {
-    ssl: true,
-  },
 };
