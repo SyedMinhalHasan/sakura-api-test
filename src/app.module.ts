dfc4aee438c88b64512f2c5be0b17005085fab62
Syed Minhal Hasan <minhalhasan9@gmail.com>
2021-02-12 16:32:26 +0500

    config2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'ec2-174-129-255-46.compute-1.amazonaws.com',
-      port: 5432,
-      username: 'keypoamxokrnvw',
-      password:
-        '01af17f2175f07e74a4f9cbe18599e1566b855f90f13f640b587676e07f380d9',
-      database: 'd6aql6t7nf8sf0',
-      extra: {
-        ssl: true,
-      },
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
