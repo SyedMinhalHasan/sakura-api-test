3fc741443df3ca001ab7ab867ac1935be3f03417
Syed Minhal Hasan <minhalhasan9@gmail.com>
2021-02-12 18:00:41 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,6 @@ import { typeOrmConfig } from './config/typeorm.config';
       type: 'postgres',
       url:
         'postgres://keypoamxokrnvw:01af17f2175f07e74a4f9cbe18599e1566b855f90f13f640b587676e07f380d9@ec2-174-129-255-46.compute-1.amazonaws.com:5432/d6aql6t7nf8sf0',
-      entities: ['dist/models/**/*.js'],
-      synchronize: true,
-      migrations: ['dist/src/database/migrations/*.js'],
-      cli: {
-        migrationsDir: 'src/database/migrations',
-      },
     }),
   ],
   controllers: [AppController],
