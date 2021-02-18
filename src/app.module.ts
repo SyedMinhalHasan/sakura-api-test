c3753522c40fdcb11396a3bcb49ddf4eccc601d8
Syed Minhal Hasan <minhalhasan9@gmail.com>
2021-02-18 12:16:27 +0500

    config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as config from 'config';
 import { typeOrmConfig } from './config/typeorm.config';
 
+const db = config.get('db');
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url:
-        'postgres://keypoamxokrnvw:01af17f2175f07e74a4f9cbe18599e1566b855f90f13f640b587676e07f380d9@ec2-174-129-255-46.compute-1.amazonaws.com:5432/d6aql6t7nf8sf0',
+      url: db,
+      entities: ['dist/models/**/*.js'],
+      synchronize: true,
+      migrations: ['dist/src/database/migrations/*.js'],
+      cli: {
+        migrationsDir: 'src/database/migrations',
+      },
     }),
   ],
   controllers: [AppController],
